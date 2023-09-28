60c7646149f2885014b55f078d5f77c2e9b64f10
Soumyadep Dandapat <soumyadeepdandpat@gmail.com>
2023-09-28 22:53:54 +0530

    state_issue

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -9,11 +9,66 @@ class cartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: myTheme.lightpurple,
+      backgroundColor: context.theme.cardColor,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: "Cart".text.color(Colors.black).make(),
+        backgroundColor: context.theme.canvasColor,
+        title: "Cart".text.color(context.theme.hintColor).make(),
       ),
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "Rs 1000".text.xl5.color(context.theme.hintColor).make(),
+          30.widthBox,
+          ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          context.theme.highlightColor)),
+                  child: "Buy".text.color(context.theme.hintColor).make())
+              .w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline_outlined),
+            onPressed: () {},
+          ),
+          title: "Item 1".text.color(context.theme.hintColor).make()),
     );
   }
 }
